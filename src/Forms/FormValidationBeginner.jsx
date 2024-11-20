2ddda95dc9e36ae05585fa48e9ba197679a9caed
Hicham1970 <devhicham2@gmail.com>
2024-11-20 22:57:11 +0100

    Form Validation for beginners

--- src/Forms/FormValidationBeginner.jsx
+++ src/Forms/FormValidationBeginner.jsx
@@ -1,54 +1,135 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useRef, useState } from "react"
 
 function FormValidationBeginner() {
-    return (
-        <div className='container-fluid w-75 mx-auto my-5'>
-            <form>
-                <h1>Contact Form</h1><hr />
-                {/* /**------Name input------- */}
-                <div className='form-outline mb-4'>
-                    <label className='form-label' htmlFor='name'>Name</label>
-                    <input type='text' className='form-control' id='name' />
-                </div>
-                {/* /**------Email input------- */}
-                <div className='form-outline mb-4'>
-                    <label className='form-label' htmlFor='email'>Email</label>
-                    <input type='text' className='form-control' id='email' />
-                </div>
-                {/* /**------Message input------- */}
-                <div className='form-outline mb-4'>
-                    <label className='form-label' htmlFor='message'>Message</label>
-                    <textarea className='form-control' id='message'></textarea>
-                </div>
-                {/* /**------Select country input------- */}
-                <div className="form-group">
-                    <label>Country</label>
-                    <label htmlFor="country" className="form-label"></label>
-                    <select id="country" className="form-control" >
-                        <option value="fr">France</option>
-                        <option value="Mr">Morocco</option>
-                        <option value="al">Algeria</option>
-                        <option value="tu">Tunisia</option>:
-                    </select>
-                </div>
-                {/* /**------Checkbox------- */}
-                <div className="form-check my-2">
-                    <input
-                        type="checkbox"
-                        id="accept"
-                        className="form-check-input"
+  // recuperation des champs du formulaire:
+  const NameRef = useRef()
+  const EmailRef = useRef()
+  const MessageRef = useRef()
+  const CountryRef = useRef()
+  const AcceptRef = useRef()
+  const [errors, setErrors] = useState([])
+  // fonction de validation du formulaire:
+  const validateForm = () => {
+    // recuperation des valeurs des champs:
+    const name = NameRef.current.value
+    const email = EmailRef.current.value
+    const message = MessageRef.current.value
+    const country = CountryRef.current.value
+    const accept = AcceptRef.current.checked
+    let isFormValid = true
 
-                    />
-                    <label htmlFor="accept" className="form-check-label">
-                        Accept our rules
-                    </label>
-                </div>
-                <div className="form-group  my-4">
-                    <button type="submit" className="btn btn-primary w-75 mx-auto" >Submit</button>
-                </div>
-            </form>
+    // validation des champs:
+    if (name.trim() === "") {
+      setErrors(previous => {
+        return [...previous, "Name is required"]
+      })
+      isFormValid = false
+    }
+    if (email.trim() === "") {
+      setErrors(previous => {
+        return [...previous, "Email is required"]
+      })
+      isFormValid = false
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setErrors(previous => {
+        return [...previous, "Email is not valid"]
+      })
+      isFormValid = false
+    }
+    if (message.trim() === "") {
+      setErrors(previous => {
+        return [...previous, "Message is required"]
+      })
+      isFormValid = false
+    }
+    if (country.trim() === "") {
+      setErrors(previous => {
+        return [...previous, "Country is required"]
+      })
+      isFormValid = false
+    }
+    if (!accept) {
+      setErrors(previous => {
+        return [...previous, "You must accept our rules"]
+      })
+      isFormValid = false
+    }
+
+    return isFormValid
+
+  }
+  // fonction de soumission du formulaire:
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrors([])
+    validateForm()
+
+  }
+  return (
+    <div className="container-fluid w-75 mx-auto my-5">
+      {errors.length > 0 ?
+        <div className="alert alert-danger" role="alert">
+          <strong>Attention Error</strong>
+          <ul>
+            {errors.map((error, index) => <li key={index}>{error}</li>)}
+          </ul>
         </div>
-    )
+        : ""}
+      <form onSubmit={handleSubmit}>
+        <h1>Contact Form</h1>
+        <hr />
+        {/* /**------Name input------- */}
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input type="text" className="form-control" id="name" ref={NameRef} />
+        </div>
+        {/* /**------Email input------- */}
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input type="text" className="form-control" id="email" ref={EmailRef} />
+        </div>
+        {/* /**------Message input------- */}
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="message" >
+            Message
+          </label>
+          <textarea className="form-control" id="message" ref={MessageRef}></textarea>
+        </div>
+        {/* /**------Select country input------- */}
+        <div className="form-group">
+          <label>Country</label>
+          <label htmlFor="country" className="form-label"></label>
+          <select id="country" className="form-control" ref={CountryRef}>
+            <option value="fr">France</option>
+            <option value="Mr">Morocco</option>
+            <option value="al">Algeria</option>
+            <option value="tu">Tunisia</option>:
+          </select>
+        </div>
+        {/* /**------Checkbox------- */}
+        <div className="form-check my-2">
+          <input type="checkbox" id="accept" className="form-check-input" ref={AcceptRef} />
+          <label htmlFor="accept" className="form-check-label" >
+            Accept our rules
+          </label>
+        </div>
+        <div className="form-group  my-4">
+          <button
+            type="submit"
+            className="btn btn-warning fw-bold w-75 mx-auto"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default FormValidationBeginner
+export default FormValidationBeginner;
