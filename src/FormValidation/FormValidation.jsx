32a63a48e5301848618d6e7f9aabe8d4a5b94a04
Hicham1970 <devhicham2@gmail.com>
2024-11-28 23:10:01 +0100

    Avant l'épuration du code

--- src/FormValidation/FormValidation.jsx
+++ src/FormValidation/FormValidation.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-import { useRef, useState, useEffect  } from "react";
+import { useRef, useState, useEffect,useCallback  } from "react";
 
 function FormValidation() {
-  const NameRef = useRef();
-  const EmailRef = useRef();
-  const MessageRef = useRef();
-  const AcceptRef = useRef();
+  const name = useRef({});
+  const email = useRef({});
+  const message = useRef({});
+  const accept = useRef({});
   const [isFormSent, setIsFormSent] = useState(false);
   const [error, setError] = useState({})
-  
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   // fonction pour vider le formulaire:
   const resetForm=()=>{
-    NameRef.current.value=""
-    EmailRef.current.value=""
-    MessageRef.current.value=""
-    AcceptRef.current.checked=false
+    name.current.value=""
+    email.current.value=""
+    message.current.value=""
+    accept.current.checked=false
 
   }
-  // fonction pour afficher les erreurs:
   
   // fonction pour valider le formulaire:
-  const validateForm=()=>{
+  const validateForm=useCallback(()=>{
     setError([])
-    const nameValue = NameRef.current.value
-    const emailValue = EmailRef.current.value
-    const messageValue = MessageRef.current.value
-    const acceptValue = AcceptRef.current.checked
+    const nameValue = name.current.value
+    const emailValue = email.current.value
+    const messageValue = message.current.value
+    const acceptValue = accept.current.checked
     let isValid = true
     // valider le champ du name:
     if(nameValue.trim()===''){
     setError(prevState=>{
         return { 
           ...prevState,
-          ...{NameRef: ` NameField required`}
+          ...{name: ` NameField required`}
       } 
       })
   
@@ -43,7 +43,7 @@ function FormValidation() {
       setError(prevState => {
         return {
           ...prevState,
-          ...{EmailRef: ` EmailField required`}
+          ...{email: ` EmailField required`}
         } 
       })
     
@@ -51,7 +51,7 @@ function FormValidation() {
       } else if (!/^[A-Z0-9._%+2-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(emailValue)) {
       setError(prevState =>{return {
         ...prevState,
-        ...{EmailRef: [`This email is not correct`]}
+        ...{email: [`This email is not correct`]}
       }
       })
       isValid = false
@@ -62,10 +62,9 @@ function FormValidation() {
       setError(prevState => {
         return {
           ...prevState,
-          ...{MessageRef: ` MessageField can not be empty`}
+          ...{message: ` MessageField can not be empty`}
         }
       })
-     
       isValid = false
     }
     // valider le checkbox: 
@@ -73,38 +72,67 @@ function FormValidation() {
       setError(prevState => {
         return {
           ...prevState,
-          ...{AcceptRef: `The Checkbox must be checked`}
+          ...{accept: `The Checkbox must be checked`}
         }
       })
      
       isValid = false
     }
+    setIsSubmitted(isSubmitted=>true)
     return isValid
-  }
-  useEffect(() => {console.log(error) },[error])
-  // fonction pour handler la soumission du formulaire:
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // console.log(validateForm())
-    if (validateForm()) {
-      setIsFormSent(true)
-      resetForm()
+  },[name, email, message, accept])
+  
+  useEffect(() => {
+    if (isSubmitted) {
+      validateForm()
     }
-  };
-  // fonction pour afficher les erreurs individuelles:
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSubmitted,validateForm])
+
+  const handleChange = () => {
+    validateForm()
+   }
+  // fonction pour handler la soumission du formulaire:
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsFormSent(false);
+    if (validateForm()) {
+      setIsFormSent(true);
+      resetForm();
+      setError({})
+    }
+  }
+
+  // fonction pour afficher les erreurs individuelles se compose de 3 fonction:
+  const getError = (fieldName) => {
+    return error[fieldName]
+  }
+  const hasError = (fieldName) => {
+    return getError(fieldName) !== undefined
+  }
   const handleShowError = (fieldName) => {
-    const erreur = error[fieldName]
-    if (erreur !==undefined) {
-      return <div className={"text-muted "}>{erreur}</div>
-     }
+    // console.log(fieldName)  
+    const champs = document.querySelector(`#${fieldName}`)
+    if (hasError(fieldName)) {
+      champs.style.border = "1px solid red"
+      return <div className="text-muted ">{getError(fieldName)}</div>
+    }
+    if (champs !== undefined && champs !== null) {
+      document.querySelector(`#${fieldName}`).removeAttribute("style")
+}
    }
   // fonction pour afficher les erreurs:
   const displayErrors = () => {
+    if (Object.keys(error).length === 0 && Object.values(error).every(val => val === undefined)) {
+      return null; // ou un élément vide
+    }
     return Object.entries(error).map((err) => {
       const [field, message] = err
       return <li key={field}>{field}:{message}</li>
     })
   }
+ 
+
   return (
     <div className="container-fluid w-75 mx-auto my-5">
       {/* /**------Error message------- */}
@@ -134,8 +162,8 @@ function FormValidation() {
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input type="text" className="form-control" id="name" ref={NameRef} />
-          {handleShowError("NameRef")}
+          <input type="text" className="form-control" id="name" ref={name} onChange={handleChange} />
+          {handleShowError("name")}
         </div>
         {/* /**------Email input------- */}
         <div className="form-outline mb-4">
@@ -146,9 +174,10 @@ function FormValidation() {
             type="text"
             className="form-control"
             id="email"
-            ref={EmailRef}
+            ref={email}
+            onChange={handleChange}
           />
-          {handleShowError("EmailRef")}
+          {handleShowError("email")}
         </div>
         {/* /**------Message input------- */}
         <div className="form-outline mb-4">
@@ -158,9 +187,10 @@ function FormValidation() {
           <textarea
             className="form-control"
             id="message"
-            ref={MessageRef}
+            ref={message}
+            onChange={handleChange}
           ></textarea>
-          {handleShowError("MessageRef")}
+          {handleShowError("message")}
         </div>
 
         {/* /**------Checkbox------- */}
@@ -169,16 +199,18 @@ function FormValidation() {
             type="checkbox"
             id="accept"
             className="form-check-input"
-            ref={AcceptRef}
+            ref={accept}
+            onChange={handleChange}
           />
           <label htmlFor="accept" className="form-check-label">
             Accept our rules
           </label>
-          {handleShowError("AcceptRef")}
+          {handleShowError("accept")}
         </div>
         <div className="form-group  my-4">
           <button
             type="submit"
+            disabled={!isSubmitted}
             className="btn btn-warning fw-bold w-75 mx-auto"
           >
             Submit
